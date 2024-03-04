8d89686517b8b28aad254ebfbf44d9324c62004c
Samuel Folledo <samuelfolledo@gmail.com>
2024-03-04 14:14:48 -0700

    Commit 1: Create and show an empty GameView and GameOverView     - Created views and view model     - Add a way to transition from `HomeView` to `GameView` to `GameOverView` and popBack to `HomeView` by changing HomeView’s `NavigationView` into `NavigationStack` and using `NavigationPath`

---
+++ iOS/FuFight/FuFight/Game/GameOver/GameOverViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  GameOverViewModel.swift
+//  FuFight
+//
+//  Created by Samuel Folledo on 3/4/24.
+//
+
+import SwiftUI
+
+@Observable
+class GameOverViewModel: BaseViewModel {
+
+    override func onAppear() {
+        super.onAppear()
+    }
+
+    //MARK: - Public Methods
+}
+
+//MARK: - Private Methods
+private extension GameOverViewModel {
+
+}
