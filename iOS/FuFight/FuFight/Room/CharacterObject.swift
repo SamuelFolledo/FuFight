1edeaa39e10a28fbbbfdcf158962953312511210
Samuel Folledo <samuelfolledo@gmail.com>
2024-07-25 20:21:40 -0700

    Commit 68: Add a way to keep track of purchased fighters (7/24-7/25)     - Add `PurchaseStatus` enum to keep track of `CharacterObject`'s state         - upcoming, locked, unlocked, selected         - Initially made all characters in RoomView have the default status of locked     - Add a way to run different actions based on PurchaseStatus     - Load RoomView’s characters with randonm PurchaseStatus     - Sort characters based on PurchaseStatus

--- iOS/FuFight/FuFight/Room/CharacterObject.swift
+++ iOS/FuFight/FuFight/Room/CharacterObject.swift
@@ -7,16 +7,41 @@
 
 import Foundation
 
-struct CharacterObject: Identifiable, Hashable {
+enum PurchaseStatus: Int {
+    case upcoming
+    case locked
+    case unlocked
+    case selected
+
+    var sortOrder: Int {
+        switch self {
+        case .upcoming:
+            2
+        case .locked:
+            1
+        case .unlocked:
+            0
+        case .selected:
+            0
+        }
+    }
+}
+
+struct CharacterObject: Identifiable, Hashable, Comparable {
     var fighterType: FighterType
+    var status: PurchaseStatus
 
     var id: String { fighterType.rawValue }
+
+    public func hash(into hasher: inout Hasher) {
+        return hasher.combine(id)
+    }
 
     static func == (lhs: CharacterObject, rhs: CharacterObject) -> Bool {
         return lhs.id == rhs.id
     }
 
-    public func hash(into hasher: inout Hasher) {
-        return hasher.combine(id)
+    static func < (lhs: CharacterObject, rhs: CharacterObject) -> Bool {
+        return lhs.status.sortOrder < rhs.status.sortOrder
     }
 }
